7ce2c05a886dc72136ab40c52bf1102aae174c9c
haruna <news.sigeak@gmail.com>
2025-04-26 09:09:03 +0900

    リモートリポジトリの変更

---
+++ src/php01/index03.php
@@ -0,0 +1,14 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+?>
+
+<?php
+$name = "Tanaka" ;
+$last_name = "Yamada";
+$first_name = "Saburo";
+
+echo $name . "Jiro";
+echo "<br />";
+echo $last_name . $first_name;
+?>
